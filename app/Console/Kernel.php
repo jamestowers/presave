7d38953383e615634c3db5c714f8541b4f7fd14d
James Towers <james@songdrop.com>
2017-03-10 10:16:13 +0000

    do the release add check every 10 mins

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('fans:addReleases')
-            ->everyMinute();
+            ->everyTenMinutes();
     }
 
     /**
